92856a8c71d19ef82f498f3c6b12a465073d0ebb
Jack O'Brien <Johnob2016@gmail.com>
2014-09-15 13:46:22 -0400

    Server is \"functional\"

--- uRelayServer.py
+++ uRelayServer.py
@@ -10,6 +10,9 @@ def broadcast_message(sock, message):
 				socket.close()
 				connected_sockets.remove(socket)
 
+				print "[%s:%s] disconected\n" % addr
+
+# Main Method
 if __name__ == "__main__":
 
 	connected_sockets = []
@@ -18,39 +21,46 @@ if __name__ == "__main__":
 
 	# Creates IPv4 socket using TCP
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 	server_ip = socket.gethostbyname(socket.gethostname())
-	server_socket.bind((server_ip, PORT))
+	server_socket.bind(("0.0.0.0", PORT))
 	server_socket.listen(10)
+
+	connected_sockets.append(server_socket)
+
+	print "Server started on %s:%d" % (server_ip, PORT)
 
 	while 1:
 		read_sockets, write_sockets, err_sockets = select.select(
-			conneded_sockets, [], [])
+			connected_sockets, [], [])
 
-		for sock in read_socks:
+		for sock in read_sockets:
 			
 			# New Connection
 			if sock == server_socket:
 				new_socket, addr = server_socket.accept()
-				connected_sockets.append(sockfd)
+				connected_sockets.append(new_socket)
 				
 				# Why are there two %s
-				print "Client (%s, %s) connected" % addr
+				print "Client (%s, %s) connected\n" % addr
 
-				broadcast_message(sockfd, "[%s:%s] connected\n" % addr)
+				broadcast_message(new_socket, "[%s:%s] connected\n" % addr)
 
 			# Incoming message
 			else:
 				try:
 					message = sock.recv(BUFFER)
 
-					if data:
+					if message:
 						name = "\n<" + str(sock.getpeername()) + '> '
 						broadcast_message(sock, name + message)
 				except:
 					broadcast_message(sock, "[%s:%s] disconected" % addr)
-					print "Client (%s, %s) disconected" % addr
+					print "Client (%s, %s) disconected\n" % addr
 					
-					sock.close()
-					connected_sockets.remove(sock)
+					if sock in connected_sockets:
+						sock.close()
+						connected_sockets.remove(sock)
+
 	server_socket.close()
