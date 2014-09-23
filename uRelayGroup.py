43283361611b755db105178b5e127acf4cda8789
Jack O'Brien <obrienjo@mail.gvsu.edu>
2014-09-22 23:01:46 -0400

    Commented and cleaned up Groups

--- uRelayGroup.py
+++ uRelayGroup.py
@@ -1,39 +1,41 @@
-from random import randint
-
 class Group(object):
 	
-	group_name = "Group #%d" % randint(1, 512)
 	group_users = {}
 
 	# Constructor
 	def __init__(self, name):
 		self.group_name = name
+		self.group_users = {}
 
+	# Adds user to group
 	def add_user(self, socket, user):
-		print "--Group add user--"
 		self.group_users[socket] = user
 		message = "~~%s joined %s~~\n" % (user, self.group_name)
 		self.group_broadcast(socket, message)
 	
+	# Removes user from group
 	def remove_user(self, socket):
-		print "--Removing user--"
 		user = self.group_users[socket]
-		print "--235213462136--"
 		message = "~~%s disconnected~~\n" % user
-		print "--REMOVE USER--"
 		self.group_broadcast(socket, message)
 		del self.group_users[socket]
 
+	# Messages the entire group
 	def group_broadcast(self, sender, message):
-		print "--Group Broadcast--"
+		print "~~Broadcasting to %s: %s" % (
+			self.group_name, self.group_users[sender])
 		for socket in self.group_users:
 			if socket != sender:
 				try:
-					print "--Trying to send message--"
 					socket.send("\n{%s} %s" % (
 						self.group_users[sender], message))
 				except:
-					print "--Trying to remove user--"
 					self.remove_user(socket)
+
+	# Returns the name of this group
 	def getName(self):
 		return self.group_name
+
+	# Retuns the list of users in this group
+	def getUsers(self):
+		return self.group_users
